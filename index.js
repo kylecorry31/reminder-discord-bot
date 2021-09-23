a168fb9a2c6522b09d6cdfa21c80a443489d0ffb
Kyle Corry <kylecorry31@gmail.com>
2021-09-23 08:52:30 -0400

    Store events in redis

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const Discord = require("discord.js");
 var schedule = require("node-schedule");
-const { meetings } = require("./meetings.json");
 const { token } = require("./token.json");
+const redis = require("redis");
+const redisClient = redis.createClient({host: 'redis'});
 
-console.log(meetings);
+const createMessageRegex = /!create\s+([0-9a-zA-Z-]+)\s+\'([\d\*]+\s[\d\*]+\s[\d\*]+\s[\d\*]+\s[\d\*]+)\'\s(.*)/g;
+
+const jobMap = {};
+
+redisClient.on("error", function(error) {
+  console.error(error);
+});
+
+redisClient.get("name", redis.print);
 
 const client = new Discord.Client();
 
@@ -25,29 +34,102 @@ function sendMessage(message) {
 
 client.login(token);
 
+loadEvents();
+
 client.on('message', msg => {
-    if (msg.content === 'ping') {
+    if (msg.content === '!ping') {
         msg.reply('pong');
     }
 
     if (msg.content === '!events'){
-      let meetingString = "";
-      for (meeting of meetings){
-        meetingString += `${meeting.name}\n\t\t ${meeting.repeat ? 'Every' : ''} ${meeting.days.join(',')} at ${meeting.time}\n\n`;
-      }
+      getEvents(msg);
+    }
 
-      msg.reply(meetingString.trim());
+    if (msg.content.startsWith("!create")){
+      const createMatches = [...msg.content.matchAll(createMessageRegex)];
+      if (createMatches.length > 0){
+        addEvent(createMatches[0][1], createMatches[0][2], createMatches[0][3], msg);
+      }
+    }
+
+    if (msg.content.startsWith("!get")){
+      const split = msg.content.split(' ');
+      if (split.length > 0){
+        const key = split[1];
+        getEvent(key, msg);
+      }
+    }
+
+    if (msg.content.startsWith("!delete")){
+      const split = msg.content.split(' ');
+      if (split.length > 0){
+        const key = split[1];
+        removeEvent(key, msg);
+      }
     }
 });
 
-// TODO: Parse the date and time strings
-meetings.forEach(meeting => {
-  console.log(`Meeting ${meeting.name} scheduled for ${meeting.reminder.when}`);
-  schedule.scheduleJob(meeting.name + " - reminder", meeting.reminder.when, () => {
-    sendMessage('@everyone ' + meeting.reminder.message);
+function loadEvents(){
+  redisClient.lrange('events', 0, -1, (err, reply) => {
+    reply.forEach((key) => {
+      redisClient.get(key, (err, reply) => {
+        if (reply){
+          const json = JSON.parse(reply);
+          console.log(json);
+          scheduleEvent(key, json.cron, json.message);
+        }
+      });
+    });
+  });
+}
+
+function addEvent(key, cron, message, msg){
+  redisClient.set(key, JSON.stringify({cron, message}), () => {
+    msg.reply(`${key} was created with schedule: ${cron}`);
+    scheduleEvent(key, cron, message);
+  });
+  redisClient.lrem('events', 0, key, () => {});
+  redisClient.rpush('events', key, () => {});
+}
+
+function removeEvent(key, msg){
+  redisClient.del(key, (err, ok) => {
+    if (ok){
+      msg.reply(`${key} was deleted`);
+      redisClient.lrem('events', 0, key, () => {});
+      unscheduleEvent(key);
+    }
+  });
+}
+
+function getEvent(key, msg){
+  redisClient.get(key, (err, reply) => {
+    msg.reply(`${key}: ${reply}`);
+  });
+}
+
+function getEvents(msg){
+  redisClient.lrange('events', 0, -1, (err, reply) => {
+    if (reply.length == 0){
+      msg.reply('No events');
+    }
+    reply.forEach((key) => {
+      getEvent(key, msg);
+    });
+  });
+}
+
+function scheduleEvent(key, cron, message){
+  unscheduleEvent(key);
+  console.log(`Scheduling ${key} for ${cron}`);
+  const job = schedule.scheduleJob(key, cron, () => {
+    sendMessage('@everyone ' + message);
   });
 
-  schedule.scheduleJob(meeting.name + " - upcoming reminder", meeting.upcoming_reminder.when, () => {
-    sendMessage('@everyone ' + meeting.upcoming_reminder.message);
-  });
-});
+  jobMap[key] = job;
+}
+
+function unscheduleEvent(key){
+  const job = jobMap[key];
+  job?.cancel();
+}
